9cdab1ead2e8caa633c8d33f462aa4f70c5c9657
gzenner <gzenner@student.42luxembourg.lu>
2025-05-06 16:45:11 +0200

    files

--- index.py
+++ index.py
@@ -3,3 +3,20 @@ from pages.BasicModule import BasicModule
 from pages.AdvancedModule import AdvancedModule
 
 st.header("Welcome to the Python Holy Graph!")
+
+newemail = st.text_input("Enter Your Email For Newsletter ")
+
+if st.button("Sign Up For Newsletter"):
+    known_emails = open("emails.txt", "r").read()
+    #known_emails = known_emails.read()
+    print(f"[debug known emails]{known_emails}")
+    file = open("emails.txt", "a")
+    if newemail not in known_emails:# and len(newemail) > 3 and '@' in newemail:
+        file.write(f"{newemail}\n")
+
+#ADD EMAIL SIGNUP For Newsletter
+#ADD a textinput so user can enter his email
+#APPEND the email to a emails.txt
+
+#research st.textinput
+#add button
